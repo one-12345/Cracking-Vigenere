d1d92ddbf5ae49d3ddb7d96c4eced8c7e42b0715
one-12345 <michaelyang800@gmail.com>
2026-04-08 11:23:31 -0400

    Initial Commit

---
+++ VigenereCracker.java
@@ -0,0 +1,21 @@
+class VigenereCracker {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] keyLengthFinder(String text) {
+        int[] likelyKeyLengths = new int[10];
+        return likelyKeyLengths;
+    }
+
+    public String keyFinder(String text, int keyLength) {
+        String key = "";
+        return key;
+    }
+
+    public int similarityFinder(String text, String key) {
+        int similarityScore = 0;
+        return similarityScore;
+    }
+
+}
